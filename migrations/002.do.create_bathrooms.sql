6b38c427564a4b300932c3587fd62cc64f2fb590
flitterio <francesca@litterio.net>
2021-04-06 17:12:56 -0400

    will this work?

--- migrations/002.do.create_bathrooms.sql
+++ migrations/002.do.create_bathrooms.sql
@@ -6,5 +6,11 @@ CREATE TABLE bathrooms (
     description TEXT,
     user_id INTEGER
         REFERENCES users(id) ON DELETE CASCADE NOT NULL,
-    category kind_of_type NOT NULL
+    category kind_of_type NOT NULL,
+    ishandicap BOOLEAN DEFAULT false,
+    isfamily BOOLEAN DEFAULT false,
+    hasstalls BOOLEAN DEFAULT false,
+    isprivate BOOLEAN DEFAULT false,
+    gender_neutral BOOLEAN DEFAULT false,
+    hasbaby_table BOOLEAN DEFAULT false
 );
---
+++ seeds/unseed.pnyc_tables.sql
@@ -0,0 +1,5 @@
+DELETE FROM users;
+DELETE FROM bathrooms;
+DELETE FROM rates;
+DELETE FROM favorites;
+DELETE FROM comments;
