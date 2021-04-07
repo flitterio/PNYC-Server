17cc9f3083bbff37d8a59819f17d705a313a3aa5
flitterio <francesca@litterio.net>
2021-04-07 13:41:27 -0400

    fix server

--- src/server.js
+++ src/server.js
@@ -3,9 +3,14 @@ const app = require('./app')
 const { PORT, DATABASE_URL} = require('./config')
 
 const db = knex({
-  client: 'pg',
-  connection: DATABASE_URL,
-})
+  client: 'pg', 
+  connection: { 
+    connectionString: DATABASE_URL,
+     ssl: { 
+       rejectUnauthorized: false 
+      } 
+    } 
+});
 
 app.set('db', db)
 
