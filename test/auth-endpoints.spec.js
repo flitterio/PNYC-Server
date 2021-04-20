3003a204c5b5b7284e4b21bf7c63fbd416994e0b
flitterio <francesca@litterio.net>
2021-04-20 16:26:21 -0400

    fixed tests

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -23,7 +23,7 @@ describe('Auth Endpoints', function() {
 
   afterEach('cleanup', () => helpers.cleanTables(db))
 
-  describe(`POST /api/auth/login`, () => {
+  describe(`POST /api/auth/signin`, () => {
     beforeEach('insert users', () =>
       helpers.seedUsers(
         db,
@@ -43,7 +43,7 @@ describe('Auth Endpoints', function() {
         delete loginAttemptBody[field]
 
         return supertest(app)
-          .post('/api/auth/login')
+          .post('/api/auth/signin')
           .send(loginAttemptBody)
           .expect(400, {
             error: `Missing '${field}' in request body`,
@@ -54,7 +54,7 @@ describe('Auth Endpoints', function() {
     it(`responds 400 'invalid username or password' when bad username`, () => {
       const userInvalidUser = { username: 'user-not', password: 'existy' }
       return supertest(app)
-        .post('/api/auth/login')
+        .post('/api/auth/signin')
         .send(userInvalidUser)
         .expect(400, { error: `Incorrect username or password` })
     })
@@ -62,7 +62,7 @@ describe('Auth Endpoints', function() {
     it(`responds 400 'invalid username or password' when bad password`, () => {
       const userInvalidPass = { username: testUser.username, password: 'incorrect' }
       return supertest(app)
-        .post('/api/auth/login')
+        .post('/api/auth/signin')
         .send(userInvalidPass)
         .expect(400, { error: `Incorrect username or password` })
     })
@@ -74,21 +74,11 @@ describe('Auth Endpoints', function() {
         username: testUser.username,
         password: testUser.password,
       }
-      const expectedToken = jwt.sign(
-        { user_id: testUser.id },
-        process.env.JWT_SECRET,
-        {
-          subject: testUser.username,
-          expiresIn: process.env.JWT_EXPIRY,
-          algorithm: 'HS256',
-        }
-      )
+
       return supertest(app)
-        .post('/api/auth/login')
+        .post('/api/auth/signin')
         .send(userValidCreds)
-        .expect(200, {
-          authToken: expectedToken,
-        })
+        .expect(200)
     })
   })
 
